4b8f987e54df278d3c596f956450272b6524c3e3
Burkee504 <oisburke1@gmail.com>
2024-07-13 16:43:12 +0000

    Commit 1

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "mbed.h"
+// Question 5 CA Embeeded systems. Everytime the button PC13 is pressed print the message button pressed 
+
+DigitalIn button1(PC_13);
+int pressCount = 0; 
+int main()
+{
+    while (1){
+    if (!button1) {
+    pressCount ++ ; 
+    printf ("Button 1 is pressed! Total presses: %d/n", pressCount);
+    }
+
+    {
+
+    }
+}
+
